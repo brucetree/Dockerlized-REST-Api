b0b71ceace72a68c3b73e047060d80eac0c36366
brucetree <bruceliu@Bruces-MBP.modem>
2023-08-26 22:49:35 +1000

    initiliaze the aaplication

---
+++ app.test.js
@@ -0,0 +1,61 @@
+import axios from "axios";
+// import getJsonData from "./app";
+
+// jest.mock("axios");
+const url = "http://127.0.0.1:3000";
+
+function isArrayOfObjectsAscending(arr, key) {
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i][key] < arr[i - 1][key]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function doObjectsHaveKey(arr, key) {
+  for (const obj of arr) {
+    if (!(key in obj)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+describe("fetch data", () => {
+  test("when I fetch data from local IP I can get 200 response", async () => {
+    const res = await axios.get(url);
+
+    expect(res).toBeTruthy();
+    expect(res.status).toBe(200);
+  });
+
+  test("fetched data should be ascending according to tempture", async () => {
+    const res = await axios.get(url);
+    expect(isArrayOfObjectsAscending(res.data, "apparent_t")).toBeTruthy();
+  });
+
+  test("fetched data should be have name, apparent_t,lat and long ", async () => {
+    const res = await axios.get(url);
+    expect(doObjectsHaveKey(res.data, "apparent_t")).toBeTruthy();
+    expect(doObjectsHaveKey(res.data, "name")).toBeTruthy();
+    expect(doObjectsHaveKey(res.data, "lat")).toBeTruthy();
+    expect(doObjectsHaveKey(res.data, "long")).toBeTruthy();
+  });
+
+  // test("handles API error with specific response", async () => {
+  //   const errorResponse = {
+  //     response: {
+  //       status: 503,
+  //       data: {
+  //         error: "Error Connecting to BOM.",
+  //       },
+  //     },
+  //   };
+
+  //   axios.get.mockRejectedValue(); // Mock the API error
+
+  //   await expect(getJsonData()).rejects.toEqual(errorResponse);
+  // });
+});
