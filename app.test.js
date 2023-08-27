c898e3ba691c4a39a996d71dd76c056fb58cc859
brucetree <{brucetree}@users.noreply.github.com>
2023-08-27 17:56:11 +1000

    solve issue

--- app.test.js
+++ app.test.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://127.0.0.1:8000";
+const url = "http://127.0.0.1:3000";
 
 // function to check array of object ascending order
 function isArrayOfObjectsAscending(arr, key) {
