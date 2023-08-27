6d2751554fc6f997b0dedd765e42e2c4bd963b0e
brucetree <{brucetree}@users.noreply.github.com>
2023-08-27 18:58:29 +1000

    CORS

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(
   cors({
       origin: "http://localhost:3000", 
       credentials: true,
+      methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE']
   })
 );
 const port = process.env.PORT || 3000;
